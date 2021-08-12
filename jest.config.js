68d42129174d1c538fe45bcf1f746a1b1f88023b
Interbred <interbred.monkey@gmail.com>
2021-08-12 09:22:59 +0100

    chore(missing config): added jest config and ts-jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  testTimeout: 45000,
+  moduleNameMapper: {
+    'source-map-support/register': 'identity-obj-proxy',
+  },
+};
